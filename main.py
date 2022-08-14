1ac21c9db5ee1060b80f17c9731c27ded408aace
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-08-14 21:59:04 +0300

    Add image opener Add config

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from visualizer import Visualizer
 
+
+def shuffle(vs: Visualizer):
+    pass
+
+
 if __name__ == '__main__':
-    vs = Visualizer()
+    data = Visualizer()
+    # Алгоритм сортировки тут, список заполнять не нужно
+    print(len(data))
+    print(data[100])
+    # for i in range(len(data)):
+    #     for j in range(len(data) - i - 1):
+    #         if a[j] > a[j + 1]:
+    #             a[j], a[j + 1] = a[j + 1], a[j]
--- visualizer.py
+++ visualizer.py
@@ -8,16 +8,57 @@ import tkinter.ttk as ttk
 from tkinter import *
 from PIL import Image
 
+import config as cfg
 
-class Visualizer:
+
+class Visualizer(list):
     def __init__(self):
-        self.app_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-        self.get_images()
+        self.img_height: int
+        self.img_width: int
+        self.img = None
+        self.pixels = None
+        self.open_image()
+        self.add_background()
+        super().__init__([-1] * (self.img_width * self.img_width))
+        self.fill_list()
+        self.counter = 0
+        self.is_start = False
+
         debug('Приложение запущено')
 
-    def get_images(self) -> list:
-        for address, dirs, files in os.walk(self.app_path):
-            print(address, dirs, files)
+    def add_background(self):
+        """Добавляет вместо пустоты монотонный фон"""
+        for i in range(self.img_width):
+            for j in range(self.img_height):
+                r, g, b, a = self.pixels[i, j]
+                if a == 0:
+                    self.img.putpixel((i, j), cfg.BACKGROUND_COLOR)
+        # self.img.show()
+
+    def open_image(self):
+        try:
+            self.img = Image.open('img.png')
+            self.pixels = self.img.load()
+        except IOError:
+            print("Не удалось открыть изображение")
+            sys.exit(1)
+        self.img_width, self.img_height = self.img.size
+        print(self.img_width, self.img_height)
+
+    def fill_list(self):
+        for i in range(self.img_width * self.img_height):
+            super().__setitem__(i, i)
+
+    def get_counter(self):
+        return '{:0<5}'.format(str(self.counter))
+
+    def __getitem__(self, item):
+        if self.is_start:
+            self.save_frame()
+        return super().__getitem__(item)
+
+    def save_frame(self):
+        pass
 
 
 def debug(data: Any):
