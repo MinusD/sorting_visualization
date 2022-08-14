595c5921449e53b043804a1360e83cc855c12b4e
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-08-14 23:17:20 +0300

    Add shuffle Add set and get pixels

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import random
+
 from visualizer import Visualizer
 
 
 def shuffle(vs: Visualizer):
-    pass
+    for x in range(len(vs)):
+        i = random.randint(0, len(vs) - 1)
+        j = random.randint(0, len(vs) - 1)
+        tmp = vs[i]
+        vs[i] = vs[j]
+        vs[j] = tmp
 
 
 if __name__ == '__main__':
     data = Visualizer()
+    shuffle(data)
     # Алгоритм сортировки тут, список заполнять не нужно
     print(len(data))
-    print(data[100])
+    for i in range(len(data)):
+        print(data[i])
+    # print(data[100])
     # for i in range(len(data)):
     #     for j in range(len(data) - i - 1):
-    #         if a[j] > a[j + 1]:
-    #             a[j], a[j + 1] = a[j + 1], a[j]
+    #         if data[j] > data[j + 1]:
+    #             data[j], data[j + 1] = data[j + 1], data[j]
--- visualizer.py
+++ visualizer.py
@@ -13,18 +13,30 @@ import config as cfg
 
 class Visualizer(list):
     def __init__(self):
+        self.path = None
         self.img_height: int
         self.img_width: int
         self.img = None
         self.pixels = None
         self.open_image()
         self.add_background()
-        super().__init__([-1] * (self.img_width * self.img_width))
+        super().__init__([[-1, (0, 0, 0, 0)]] * (self.img_width * self.img_height))
+        self.last_use = [[-1, (0, 0, 0, 0)]] * (self.img_width * self.img_height)
         self.fill_list()
-        self.counter = 0
-        self.is_start = False
 
-        debug('Приложение запущено')
+        self.create_tmp_dir()
+        self.counter = 0
+        self.is_start = True
+
+        debug(
+            f'Приложение запущено\nШирина: {self.img_width}\nВысота: {self.img_height}\n'
+            f'Пиксели: {self.img_width * self.img_height} - {super().__len__()}')
+
+    def create_tmp_dir(self) -> None:
+        """Создаёт временную директорию"""
+        app_path = os.path.dirname(os.path.realpath(sys.argv[0]))  # Папка, откуда запускаемся
+        self.path = os.path.join(app_path, 'tmp')
+        os.makedirs(self.path, exist_ok=True)
 
     def add_background(self):
         """Добавляет вместо пустоты монотонный фон"""
@@ -33,32 +45,59 @@ class Visualizer(list):
                 r, g, b, a = self.pixels[i, j]
                 if a == 0:
                     self.img.putpixel((i, j), cfg.BACKGROUND_COLOR)
-        # self.img.show()
 
     def open_image(self):
+        """Открывает изображение и сохраняет размеры"""
         try:
-            self.img = Image.open('img.png')
+            self.img = Image.open('img2.png')
             self.pixels = self.img.load()
         except IOError:
             print("Не удалось открыть изображение")
             sys.exit(1)
         self.img_width, self.img_height = self.img.size
-        print(self.img_width, self.img_height)
 
     def fill_list(self):
+        """Заполняет список информацией о картинке"""
         for i in range(self.img_width * self.img_height):
-            super().__setitem__(i, i)
+            super().__setitem__(i, (i, self.get_pixel(i)))
+            self.last_use[i] = [i, self.get_pixel(i)]
+
+    def get_pixel(self, i: int) -> list:
+        """Массив с цветом пикселя"""
+        return [self.pixels[i // self.img_height, i % self.img_height]]
+
+    def set_pixel(self, i, rgb) -> None:
+        """Устанавливает цвет пикселя"""
+        self.img.putpixel((i // self.img_height, i % self.img_height), rgb)
 
     def get_counter(self):
-        return '{:0<5}'.format(str(self.counter))
+        """Счётчик с незначащими нулями"""
+        return '{:0>5}'.format(str(self.counter))
 
     def __getitem__(self, item):
         if self.is_start:
+            pass
+            # self.save_frame()
+
+        # print(super().__getitem__(item))
+        return super().__getitem__(item)[0]
+
+    def __setitem__(self, key, value):
+        for i in range(len(self.last_use)):
+            if self.last_use[i][0] == value:
+                super().__setitem__(key, [value, self.last_use[i][1]])
+                self.set_pixel(key, tuple(self.last_use[i][1]))
+                break
+
+        # self.set_pixel(key, 123)
+        if self.is_start:
+            # pass
             self.save_frame()
-        return super().__getitem__(item)
 
     def save_frame(self):
-        pass
+        """Сохраняет кадр"""
+        self.img.save(f'tmp\\frame-{self.get_counter()}.png')
+        self.counter += 1
 
 
 def debug(data: Any):
